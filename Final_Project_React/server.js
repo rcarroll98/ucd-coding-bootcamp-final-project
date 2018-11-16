2ca8ddce646972221939906ad451df761fb489b9
Riley Carroll <rcarroll98@gmail.com>
2018-11-15 19:16:24 -0800

    initial commit

---
+++ Final_Project_React/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+var User = require("./client/models/userSchema.js");
+
+// Configure middleware
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/userdb", { useNewUrlParser: true });
+
+// Routes
+
+// Route to post our form submission to mongoDB via mongoose
+app.post("/submit", function(req, res) {
+  // Create a new user using req.body
+  User.create(req.body)
+    .then(function(dbUser) {
+      // If saved successfully, send the the new User document to the client
+      res.json(dbUser);
+    })
+    .catch(function(err) {
+      // If an error occurs, send the error to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
+
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
